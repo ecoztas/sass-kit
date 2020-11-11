31b5141c7b9639bb17a9c20cf5f5fb5be3fa99b7
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2020-11-11 07:06:55 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /**
  * SASS-KIT for SASS development.
- * 
- * 
- * 
+ *
+ *
+ *
  * @package     sass-kit
  * @author      ÖZTAŞ, Emre Can <me@emrecanoztas.com>
  * @copyright   2020
@@ -13,30 +13,31 @@
 
 // DEFINE: Package(s).
 // =============================================================================
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
-var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cssmin = require("gulp-cssmin");
+var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
 
 // TASK: Default
 // =============================================================================
-gulp.task('default', function () {
-    console.log('Works fine!');
+gulp.task("default", function () {
+    console.log("Works fine!");
 });
 
 // TASK: SASS
 // =============================================================================
-gulp.task('sass', function () {
-    return (
-        gulp.src('./theme.scss')
-            .pipe(plumber())
-            .pipe(sass())
-            .pipe(gulp.dest('styles'))
-            .pipe(cssmin())
-            .pipe(rename({
-                suffix: '.min'
-            }))
-            .pipe(gulp.dest('styles'))
-    );
+gulp.task("sass", function () {
+    return gulp
+        .src("./theme.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("styles"))
+        .pipe(cssmin())
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(gulp.dest("styles"));
 });
